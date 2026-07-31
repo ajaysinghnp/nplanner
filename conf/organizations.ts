33a763562d4f1ae1bef68140e855bbd07046590f
Ajay Singh <ajay@nrb.org.np>
2026-07-31 09:26:03 +0545

    fix(organizations): add missing nameNe and shortNameNe fields for organizations fix: update esbuild configuration to true in pnpm workspace

--- conf/organizations.ts
+++ conf/organizations.ts
@@ -2,11 +2,15 @@ export const ORGANIZATIONS = [
   {
     code: "BRSOLUTIONS",
     nameEn: "BR Solutions",
+    nameNe: "",
     shortNameEn: "BR Solutions",
+    shortNameNe: "",
   },
   {
     code: "NPLANNER",
     nameEn: "NPlanner",
+    nameNe: "",
     shortNameEn: "NPlanner",
+    shortNameNe: "",
   },
 ];
